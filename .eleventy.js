1377480035b12ad1c85f3123864c9690b4175b07
rachitkakkar <rachitkakkar7@gmail.com>
2022-11-08 16:35:42 -0600

    Add js for compare project

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (config) {
-    // we simply tell eleventy to pass a copy of our assets folder
     config.addPassthroughCopy("./src/img");
     config.addPassthroughCopy("./src/style.css");
-    config.addPassthroughCopy("./src/CNAME"); 
+    config.addPassthroughCopy("./src/projects");
+    config.addPassthroughCopy("./src/CNAME");
+
     config.addPlugin(eleventyNavigationPlugin);
 
     return {
